7f4b295cd5b2a529ee3e0dfffdf7e783ebad7563
muhammadfachri36 <mamadfahri4@gmail.com>
2016-09-12 07:08:23 -0700

    Add files via upload

---
+++ praktikum_3_1_fix.ino
@@ -0,0 +1,40 @@
+void setup()
+{
+  DDRB=0b1111;
+  DDRC=0b0000;
+  PORTC=0b1111;
+}
+void loop()
+{
+  if (digitalRead(A0)==LOW)
+  {
+  counterup();
+  delay(1000);
+  }
+  if(digitalRead(A1)==LOW)
+  {
+    counterdown();
+    delay(1000);
+  }
+  if(digitalRead(A2)==LOW)
+  {
+    reset();
+  }
+}
+
+void counterup()
+{
+  PORTB = PORTB + 1;
+}
+void counterdown()
+{
+  PORTB = PORTB - 1;
+}
+void reset()
+{
+  PORTB = 0;
+}
+
+
+
+  
